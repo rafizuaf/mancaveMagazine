f43ca9a94168422bf7c51483981a314c59fdc47e
Mukhtar Rafi Fauzi <mukhtar.r.f@gmail.com>
2024-01-31 22:26:13 +0700

    feat: setup main and sidebar layout

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -3,10 +3,19 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <Container>
       <main className="container">
-        <p className="text-3xl font-semibold">Hello World</p>
+        <div className="flex w-full">
+          <div className="w-3/4 border-r-2 min-h-screen">
+            <div className="pt-5">
+              <p>Hellowwww</p>
+            </div>
+          </div>
+          <div className="w-1/4 pl-5">
+            <div className="pt-5">
+              <p>Sidebar</p>
+            </div>
+          </div>
+        </div>
       </main>
-    </Container>
   );
 }
--- client/components/Navbar.tsx
+++ client/components/Navbar.tsx
@@ -3,55 +3,76 @@
 import Link from "next/link";
 import { useState } from "react";
 import { FaBars, FaTimes } from 'react-icons/fa';
+import { Separator } from "./ui/separator";
 
 export default function Navbar() {
     const [nav, setNav] = useState(false);
     const handleClick = () => setNav(!nav);
 
     return (
-        <div className="relative w-full h-[80px] flex justify-between items-center px-4 text-gray-700 text-xl font-semibold border-b-2">
-            <div className="flex flex-grow h-36 object-cover justify-center">
-                <img src='/Mancave-transparent.png' alt="logo" className="object-fit" />
-            </div>
+        <div className="container">
+            <div className="relative w-full h-[80px] md:h-full flex flex-row md:flex-col justify-between items-center px-4 text-gray-700 text-xl font-semibold border-b-2">
+                <div className="flex md:-mt-5 flex-grow h-36 w-36 justify-center">
+                    <img src='/Mancave-transparent.png' alt="logo" className="object-fit" />
+                </div>
 
-            {/* Hamburger menu */}
-            <div onClick={handleClick} className={`md:hidden z-10 transform transition-opacity duration-300 ${nav ? 'opacity-70' : 'opacity-100'}`}>
-                {!nav ? <FaBars /> : <FaTimes />}
-            </div>
+                <nav>
+                    <div className="-mt-5 flex h-5 items-center space-x-4 text-md my-5">
+                        <div className="hover:border-b hover:border-red-700 px-10 transition duration-300 ease-in-out"><Link href={'/'}>CARS</Link></div>
+                        <Separator className="bg-black" orientation="vertical" />
+                        <div className="hover:border-b hover:border-red-700 px-10 transition duration-300 ease-in-out"><Link href={'/'}>MOTORCYCLES</Link></div>
+                        <Separator className="bg-black" orientation="vertical" />
+                        <div className="hover:border-b hover:border-red-700 px-10 transition duration-300 ease-in-out"><Link href={'/'}>GEARS</Link></div>
+                        <Separator className="bg-black" orientation="vertical" />
+                        <div className="hover:border-b hover:border-red-700 px-10 transition duration-300 ease-in-out"><Link href={'/'}>MUSIC</Link></div>
+                        <Separator className="bg-black" orientation="vertical" />
+                        <div className="hover:border-b hover:border-red-700 px-10 transition duration-300 ease-in-out"><Link href={'/'}>GAMING</Link></div>
+                    </div>
+                </nav>
 
-            <ul
-                className={`${
-                    !nav
+                {/* Hamburger menu */}
+                <div onClick={handleClick} className={`md:hidden z-10 transform transition-opacity duration-300 ${nav ? 'opacity-70' : 'opacity-100'}`}>
+                    {!nav ? <FaBars /> : <FaTimes />}
+                </div>
+
+                <ul
+                    className={`${!nav
                         ? 'hidden opacity-0 transition duration-300'
-                        : 'absolute transition-opacity duration-300 opacity-70 bg-white top-0 left-0 w-full h-screen flex flex-col justify-center items-center text-gray-700 transform translate-y-0'
-                } transition-opacity duration-300 ease-in-out`}
-            >
-                <li className='py-2 text-2xl border-b border-red-700'>
-                    <Link onClick={handleClick} href='home'>
-                        Home
-                    </Link>
-                </li>
-                <li className='py-2 text-2xl border-b border-red-700'>
-                    <Link onClick={handleClick} href='about'>
-                        About
-                    </Link>
-                </li>
-                <li className='py-2 text-2xl border-b border-red-700'>
-                    <Link onClick={handleClick} href='skills'>
-                        Skills
-                    </Link>
-                </li>
-                <li className='py-2 text-2xl border-b border-red-700'>
-                    <Link onClick={handleClick} href='work'>
-                        Work
-                    </Link>
-                </li>
-                <li className='py-2 text-2xl border-b border-red-700'>
-                    <Link onClick={handleClick} href='contact'>
-                        Contact
-                    </Link>
-                </li>
-            </ul>
+                        : 'absolute transition-opacity duration-300 opacity-70 bg-white top-0 left-0 w-full h-screen flex flex-col justify-center items-center text-gray-900 transform translate-y-0'
+                        } transition-opacity duration-300 ease-in-out`}
+                >
+                    <li className='py-2 text-2xl border-b border-red-700'>
+                        <Link onClick={handleClick} href='/'>
+                            Home
+                        </Link>
+                    </li>
+                    <li className='py-2 text-2xl border-b border-red-700'>
+                        <Link onClick={handleClick} href='about'>
+                            Cars
+                        </Link>
+                    </li>
+                    <li className='py-2 text-2xl border-b border-red-700'>
+                        <Link onClick={handleClick} href='skills'>
+                            Motorcycles
+                        </Link>
+                    </li>
+                    <li className='py-2 text-2xl border-b border-red-700'>
+                        <Link onClick={handleClick} href='work'>
+                            Gears
+                        </Link>
+                    </li>
+                    <li className='py-2 text-2xl border-b border-red-700'>
+                        <Link onClick={handleClick} href='contact'>
+                            Music
+                        </Link>
+                    </li>
+                    <li className='py-2 text-2xl border-b border-red-700'>
+                        <Link onClick={handleClick} href='contact'>
+                            Gaming
+                        </Link>
+                    </li>
+                </ul>
+            </div>
         </div>
     );
 }
