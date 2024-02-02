7d45f164354bf8a837d7a7f7fa9025ded3e2a5a5
Mukhtar Rafi Fauzi <mukhtar.r.f@gmail.com>
2024-02-02 20:39:22 +0700

    styling: footer responsiveness

--- client/app/(without-layout)/signup/page.tsx
+++ client/app/(without-layout)/signup/page.tsx
@@ -1,44 +1,70 @@
-import Image from "next/image"
-import Link from "next/link"
-import { cn } from "@/lib/utils"
+'use client'
 
-export default function SignUp() {
+import React from 'react';
+import Link from "next/link"
+import { useState } from "react"
+import { Input } from '@/components/ui/input';
+import { Button } from '@/components/ui/button';
+import { Label } from '@/components/ui/label';
+
+export default function Signup() {
+    const [form, setForm] = useState({ email: "", password: "" })
+
+    const handleSubmit = (e: React.FormEvent<HTMLFormElement>): void => {
+        e.preventDefault();
+    };
+
+    const handleOnChange = (e: React.ChangeEvent<HTMLInputElement>): void => {
+        const { name, value } = e.target
+        setForm((prevForm) => ({ ...prevForm, [name]: value }));
+    }
+    // console.log(form);
     return (
         <div className="w-full min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0">
             <div className="w-full sm:max-w-md p-5 mx-auto">
-                <img src="/mancave-hd-transparent.png" alt="Mancave Magazine" className="w-96 mb-4" />
-                <form>
+                <Link href="/">
+                    <img src="/mancave-hd-transparent.png" alt="Mancave Magazine" className="w-96 mb-6" />
+                </Link>
+                <form onSubmit={handleSubmit}>
                     <div className="mb-4">
-                        <label className="block mb-1 font-semibold" htmlFor="email">
+                        <Label className="block mb-3 font-semibold" htmlFor="email">
                             Email
-                        </label>
-                        <input
+                        </Label>
+                        <Input
                             id="email"
                             type="text"
                             name="email"
+                            value={form.email}
+                            onChange={(e) => {
+                                setForm((prevForm) => ({ ...prevForm, email: e.target.value }));
+                            }}
                             placeholder="Enter your email"
                             className="py-2 px-3 border border-gray-300 focus:border-red-300 focus:outline-none focus:ring focus:ring-red-200 focus:ring-opacity-50 rounded-md shadow-sm disabled:bg-gray-100 mt-1 block w-full"
                         />
                     </div>
                     <div className="mb-4">
-                        <label className="block mb-1 font-semibold" htmlFor="password">
+                        <Label className="block mb-3 font-semibold" htmlFor="password">
                             Password
-                        </label>
-                        <input
+                        </Label>
+                        <Input
                             id="password"
                             type="password"
                             name="password"
+                            value={form.password}
+                            onChange={(e) => {
+                                setForm((prevForm) => ({ ...prevForm, password: e.target.value }));
+                            }}
                             placeholder="Enter your password"
                             className="py-2 px-3 border border-gray-300 focus:border-red-300 focus:outline-none focus:ring focus:ring-red-200 focus:ring-opacity-50 rounded-md shadow-sm disabled:bg-gray-100 mt-1 block w-full"
                         />
                     </div>
                     <div className="mt-6">
-                        <button type="submit" className="w-full inline-flex items-center justify-center px-4 py-2 bg-mancave border border-transparent rounded-md font-semibold capitalize text-white/90 hover:bg-red-800 active:bg-red-800 focus:outline-none focus:border-red-800 focus:ring focus:ring-red-200 disabled:opacity-25 transition">
+                        <Button type="submit" className="w-full inline-flex items-center justify-center px-4 py-2 bg-mancave border border-transparent rounded-md font-semibold capitalize text-white/90 hover:bg-red-800 active:bg-red-800 focus:outline-none focus:border-red-800 focus:ring focus:ring-red-200 disabled:opacity-25 transition">
                             Sign Up
-                        </button>
+                        </Button>
                     </div>
-                    <div className="mt-6 text-center">
-                        <Link href="/login" className="underline hover:text-mancave transition duration-300 text-lg font-semibold">
+                    <div className="mt-3 text-center">
+                        <Link href="/login" className="underline hover:text-mancave transition duration-300 text-sm font-semibold">
                             Have an account? Login
                         </Link>
                     </div>
--- client/components/Footer.tsx
+++ client/components/Footer.tsx
@@ -5,8 +5,8 @@ export function Footer() {
         <footer className='bg-black/90'>
             <div className="bg-black/90 text-white/80 py-5">
                 <div className="mx-10 flex flex-col lg:flex-row gap-10 items-start justify-between">
-                    <div className="lg:w-1/3 mb-8 lg:mb-0">
-                        <img src="/mancave-hd-transparent.png" alt="logo" />
+                    <div className="lg:w-1/3 justify-center items-center flex min-h-full">
+                        <img src="/mancave-hd-transparent.png" alt="logo" className='lg:w-72 lg:my-12'/>
                     </div>
                     <nav className="lg:w-1/3">
                         <h2 className='text-white/80 text-center text-2xl font-semibold '>WHAT IS A MANCAVE?</h2>
@@ -18,6 +18,7 @@ export function Footer() {
                     <div className="lg:w-1/3 lg:mt-0 text-center lg:text-right">
                         <h2 className='text-white/80 text-center text-2xl font-semibold '>ABOUT US</h2>
                         <p className="text-white/40 mt-3 text-justify">Established in 2024, Mancave Magazine is the epitome of masculine elegance in the digital realm. As an independent purveyor of refined taste, we are not just a publication; we are a curated experience meticulously crafted for the modern...</p>
+                        {/* <p> man of discerning style and passion. Within these virtual pages, we celebrate the artistry of roaring engines, the allure of sleek gears, the pulse of cutting-edge gaming, and the symphony of timeless music. Mancave Magazine is the brainchild of a dedicated team, tirelessly committed to the pursuit of excellence in men's culture. We may not boast multinational proportions, but our authenticity and commitment set us apart. Join us on this journey where craftsmanship, sophistication, and the pursuit of all things extraordinary converge, redefining the essence of the modern man's lifestyle. Welcome to the Mancave – where substance meets style with unapologetic refinement.</p> */}
                         <span><Link href="/about" className="text-mancave underline">Read More</Link></span>
 
                     </div>
